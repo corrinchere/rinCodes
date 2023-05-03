74651583b6471949284a4c33c6b979754b6bf89e
corrinchere <96142555+corrinchere@users.noreply.github.com>
2023-05-02 21:23:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+document.getElementById('about-me-image').addEventListener('click', function(){
+    document.getElementById('me-personally-div').style.display="block";
+    document.getElementById('image-change').style.display="none";
+    document.getElementById('my-work-section').style.display="none";
+});
+
+document.getElementById('my-work-image').addEventListener('click', function(){
+    document.getElementById('my-work-section').style.display="block";
+    document.getElementById('image-change').style.display="none";
+    document.getElementById('me-personally-div').style.display="none";
+});
+
+
+document.getElementById('get-in-touch-image').addEventListener('click', function(){
+    document.getElementById('my-work-section').style.display="none";
+    document.getElementById('image-change').style.display="block";
+    document.getElementById('me-personally-div').style.display="none";
+});
+
+
+
+// https://www.youtube.com/watch?v=nxoHR9lltK0
